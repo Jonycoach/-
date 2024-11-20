810243b7148a4531dd431f2f32004054fc1205d8
Джони <Jonycoach1989@gmail.com>
2024-11-20 23:20:52 +0700

    Add files via upload
    homeworks_done = 12
    hours_in_work = 1.5
    faculty = "Python"
    count = 1.5/12
    print("Курс", faculty, "всего задач", homeworks_done, "затрачено часов", hours_in_work, "среднее время выполнение", count, "часа")

---
+++ variables.py
@@ -0,0 +1,5 @@
+homeworks_done = 12
+hours_in_work = 1.5
+faculty = "Python"
+count = 1.5/12
+print("Курс", faculty, "всего задач", homeworks_done, "затрачено часов", hours_in_work, "среднее время выполнение", count, "часа")
